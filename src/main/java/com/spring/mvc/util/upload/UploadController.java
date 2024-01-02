3961d79a62d240fc2e464df2a618f661ed4b3370
oh young seok <oyg9731@naver.com>
2024-01-02 09:48:45 +0900

    #45. 프로필사진 업로드 클라이언트 처리

--- src/main/java/com/spring/mvc/util/upload/UploadController.java
+++ src/main/java/com/spring/mvc/util/upload/UploadController.java
@@ -1,6 +1,7 @@
 package com.spring.mvc.util.upload;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +15,10 @@ import java.io.IOException;
 @Slf4j
 public class UploadController {
 
-    private String rootPath = "D:/spring-prj/upload";
+//    private String rootPath = "D:/spring-prj/upload";
+
+ @Value("${file.upload.root-path}")
+private String rootPath;
 
     @GetMapping("/upload-form")
     public String uploadForm(){
